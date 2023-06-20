509257a023f2674a12c3e3594c80689bac32763b
Dmitriy.Ezhov <demonex@bk.ru>
2023-06-20 12:22:32 +0400

    tring to build type AST logic

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,4 +1,5 @@
 import isObject from 'lodash/isObject.js';
+import has from 'lodash/has.js';
 import transformStringToData from './parsers.js';
 
 const compareTwoFiles = (filepath1, filepath2) => {
@@ -9,21 +10,21 @@ const compareTwoFiles = (filepath1, filepath2) => {
     const keys = Array.from(new Set([...Object.keys(data1), ...Object.keys(data2)])).sort();
 
     return keys.map((key) => {
-      const key1 = data1[key];
-      const key2 = data2[key];
-      if (key1 && key2) {
-        if (isObject(key1) && isObject(key2)) {
-          return { key, status: 'unchanged', value: iter(key1, key2) };
-        }
-        if (key1 === key2) {
-          return { key, status: 'unchanged', value: key1 };
-        }
-        return { key, status: 'changed', values: { oldValue: key1, newValue: key2 } };
+      const key1 = has(data1, key) ? data1[key] : undefined;
+      const key2 = has(data2, key) ? data2[key] : undefined;
+
+      switch (true) {
+        case isObject(key1) && isObject(key2):
+          return { key, type: 'unchanged', value: iter(key1, key2) };
+        case key1 === key2:
+          return { key, type: 'unchanged', value: key1 };
+        case key1 && !key2:
+          return { key, type: 'saved', value: key1 };
+        case !key1 && key2:
+          return { key, type: 'removed', value: key2 };
+        default:
+          return { key, type: 'changed', values: { obj1Value: key1, obj2Value: key2 } };
       }
-      if (key1) {
-        return { key, status: 'saved', value: key1 };
-      }
-      return { key, status: 'removed', value: key2 };
     });
   };
 
